96608fd17dcd648be0d5f4a9446801d9e833398e
mediocregopher <bgp2396@bellsouth.net>
2013-05-09 23:46:14 -0400

    i always forget about this....

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-mtgox "0.1"
+(defproject  org.clojars.mediocregopher/clj-mtgox "0.1"
   :description "MtGox (unofficial) api library"
   :url "http://github.com/mediocregopher/clj-mtgox"
   :license {:name "Eclipse Public License"
